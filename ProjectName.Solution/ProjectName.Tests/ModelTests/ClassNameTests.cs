4f534fc7437b72d55c9455b0529aad911c3fdf43
Student <example@example.com>
2019-07-20 12:34:52 -0700

    practice with testing

---
+++ ProjectName.Solution/ProjectName.Tests/ModelTests/ClassNameTests.cs
@@ -0,0 +1,11 @@
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using LeapYear;
+
+namespace LeapYear.Tests
+{
+  [TestClass]
+  public class LeapYearTest
+  {
+    // Test methods go here
+  }
+}
---
+++ ProjectName.Solution/ProjectName/Models/ClassName.cs
@@ -0,0 +1,7 @@
+namespace LeapYear
+{
+  public class ClassName
+  {
+    // properties, constructors, methods, etc. go here
+    }
+  }
